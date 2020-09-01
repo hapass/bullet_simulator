1f297e44861c42a1976e75ed5ab85ed7ee497943
Pavel Zavoiskikh <hapass@ya.ru>
2020-09-01 20:10:29 +0300

    Primitive shader compilation.

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -2,16 +2,18 @@
 #define GL_SILENCE_DEPRECATION
 
 #include <GLFW/glfw3.h>
-
-#define BUFFER_OFFSET(i) ((char *)NULL + (i))
+#include <iostream>
 
 int main(int argc, char** argv)
 {
+  using namespace std;
+
   if (!glfwInit())
   {
-     return -1;
+    return -1;
   }
 
+  // we don't need opengl function loader for OSX
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
@@ -20,11 +22,79 @@ int main(int argc, char** argv)
   GLFWwindow* window = glfwCreateWindow(800, 600, "Bullet manager", NULL, NULL);
   if (!window)
   {
-     glfwTerminate();
-     return -1;
+    glfwTerminate();
+    return -1;
   }
 
   glfwMakeContextCurrent(window);
+
+  float vertices[] = {
+    -0.5f, -0.5f,
+    0.5f, -0.5f,
+    0.0f, 0.5f,
+  };
+
+  // vertex buffer
+  GLuint vertexBufferId;
+  glGenBuffers(1, &vertexBufferId);
+  glBindBuffer(GL_ARRAY_BUFFER, vertexBufferId);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  // vertex shader
+  {
+    const char* vertexShaderSource = 
+    "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "  gl_Position = vec4(aPos.x, aPos.y, 0.0f, 1.0f);\n"
+    "}\0";
+
+    GLuint vertexShaderId;
+    vertexShaderId = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShaderId, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShaderId);
+
+    GLint  success;
+    char infoLog[512];
+    glGetShaderiv(vertexShaderId, GL_COMPILE_STATUS, &success);
+
+    if(!success)
+    {
+      glGetShaderInfoLog(vertexShaderId, sizeof(infoLog), NULL, infoLog);
+      std::cerr << "Vertex shader compilation failed:\n" << infoLog << std::endl;
+      glfwTerminate();
+      return -1;
+    }
+  }
+
+  // fragment shader
+  {
+    const char* fragmentShaderSource = 
+    "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "  FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\0";
+
+    GLuint fragmentShaderId;
+    fragmentShaderId = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShaderId, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShaderId);
+
+    GLint success;
+    char infoLog[512];
+    glGetShaderiv(fragmentShaderId, GL_COMPILE_STATUS, &success);
+
+    if(!success)
+    {
+      glGetShaderInfoLog(fragmentShaderId, sizeof(infoLog), NULL, infoLog);
+      std::cerr << "Fragment shader compilation failed:\n" << infoLog << std::endl;
+      glfwTerminate();
+      return -1;
+    }
+  }
 
   while (!glfwWindowShouldClose(window))
   {
